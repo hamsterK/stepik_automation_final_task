9559319e00830c47578411eb59b68f0a10ce0d17
Anastasiya Krauchuk <anastasiya.krauchuk96@gmail.com>
2022-07-30 12:03:49 +0400

    update link to product page

--- test_product_page.py
+++ test_product_page.py
@@ -2,7 +2,7 @@ from .pages.product_page import ProductPage
 
 
 def test_guest_can_add_product_to_basket(browser):
-    link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
@@ -11,7 +11,7 @@ def test_guest_can_add_product_to_basket(browser):
 
 
 def test_price_in_basket_is_correct(browser):
-    link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
