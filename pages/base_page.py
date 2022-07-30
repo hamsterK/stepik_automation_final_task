9688686137a715cf0e7f58f817094b433988ae8c
Anastasiya Krauchuk <anastasiya.krauchuk96@gmail.com>
2022-07-30 11:59:35 +0400

    add 2 testcases: should_be_correct_name_of_product_added, should_be_correct_price_in_basket

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,6 @@
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import math
 
 
 class BasePage:
@@ -16,3 +18,32 @@ class BasePage:
         except NoSuchElementException:
             return False
         return True
+
+    def return_text(self, how, what):
+        try:
+            text_value = self.browser.find_element(how, what).text
+        except NoSuchElementException:
+            return None
+        return text_value
+
+    def return_price(self, how, what):
+        try:
+            price_value = self.browser.find_element(how, what).text
+        except NoSuchElementException:
+            return None
+        return price_value
+
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,12 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
     REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
+
+
+class ProductPageLocators:
+    ADD_PRODUCT_TO_BASKET = (By.CSS_SELECTOR, 'button.btn-lg[type="submit"]')
+    PRODUCT_ADDED_SUCCESSFULLY_MESSAGE = (By.XPATH, '//*[text()[contains(.,"has been added")]]')
+    PRODUCT_NAME = (By.XPATH, '//div[@class="col-sm-6 product_main"]//h1')
+    PRODUCT_NAME_IN_ADDED_SUCCESSFULLY_MESSAGE = (By.CSS_SELECTOR, 'div.alert:nth-child(1) strong')
+    PRODUCT_PRICE_ON_PAGE = (By.XPATH, '//div[@class="col-sm-6 product_main"]//p[@class="price_color"]')
+    PRODUCT_PRICE_IN_BASKET = (By.CSS_SELECTOR, 'div.alert:nth-child(3) strong')
