09f87f11f4f2f9d5f80e59c5a6a4faeed5c8e548
Anastasiya Krauchuk <anastasiya.krauchuk96@gmail.com>
2022-07-30 15:16:57 +0400

    remove chrome logs from the output

--- conftest.py
+++ conftest.py
@@ -11,7 +11,8 @@ def pytest_addoption(parser):
 @pytest.fixture(scope='function')
 def browser(request):
     user_language = request.config.getoption('language')
-    options = Options()
+    options = webdriver.ChromeOptions()  # added to remove chrome logs
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])  # added to remove chrome logs
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
     browser = webdriver.Chrome(options=options)
     yield browser
