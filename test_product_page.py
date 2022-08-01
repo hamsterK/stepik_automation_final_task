5e624fb84a0d18c3fdee8c0a6ef0113a31542ca1
Anastasiya Krauchuk <anastasiya.krauchuk96@gmail.com>
2022-08-01 19:38:37 +0400

    add xfail for 2 cases

--- test_product_page.py
+++ test_product_page.py
@@ -30,6 +30,7 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.should_be_correct_name_of_product_added()
 
 
+@pytest.mark.xfail(reason='negative case', strict=True)
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     page = ProductPage(browser, link)
@@ -46,6 +47,7 @@ def test_guest_cant_see_success_message(browser):
     page.should_not_be_success_message()
 
 
+@pytest.mark.xfail(reason='negative case', strict=True)
 def test_message_disappeared_after_adding_product_to_basket(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     page = ProductPage(browser, link)
